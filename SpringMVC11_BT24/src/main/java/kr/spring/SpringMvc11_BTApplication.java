5bf83eacd6def9799101011e32f0f1b2c4f98d73
leejonggwon <gwon880328@gmail.com>
2026-04-18 16:31:59 +0900

    20260418_1631

--- SpringMVC11_BT24/src/main/java/kr/spring/SpringMvc11_BTApplication.java
+++ SpringMVC11_BT24/src/main/java/kr/spring/SpringMvc11_BTApplication.java
@@ -11,6 +11,7 @@ public class SpringMvc11_BTApplication {
 	public static void main(String[] args) {
 		//Spring Boot를 구동 및 실행하는 클래스 
 		SpringApplication.run(SpringMvc11_BTApplication.class, args);
+		
 	} 
 }
 
